1ea0ce00225dfdf9c191266cddaf5a15ff674bed
Gabriel0liver <gabriel.oliver77@gmail.com>
2024-03-12 14:18:30 +0300

    started tests

---
+++ src/test/java/org/example/AccountTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void depositMoney() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdrawMoney() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkBalance() {
+    }
+}
